308b51d8a4fb594cb3d9253f07a6157643812995
Illia Prykhodko <prikhodko.illiya@gmail.com>
2025-04-07 16:30:06 -0500

    treesitter fix

--- lua/djllesh/plugins/treesitter.lua
+++ lua/djllesh/plugins/treesitter.lua
@@ -2,40 +2,45 @@ return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   config = function()
     require('nvim-treesitter.install').compilers = { 'clang' }
+    -- The only way I made the config work without changing the nvim-treesitter.configs file manually
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'python',
+        'cpp',
+      },
+
+      highlight = {
+        enable = true,
+      },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-n>', -- set to `false` to disable one of the mappings
+          node_incremental = '<C-n>',
+          node_decremental = '<C-m>',
+          scope_incremental = '<C-c>',
+        },
+      },
+    }
   end,
   build = ':TSUpdate',
-  -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'diff',
-      'html',
-      'lua',
-      'luadoc',
-      'markdown',
-      'markdown_inline',
-      'query',
-      'vim',
-      'vimdoc',
-      'python',
-      'cpp',
-    },
-
-    highlight = {
-      enable = true,
-    },
-
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<C-n>', -- set to `false` to disable one of the mappings
-        node_incremental = '<C-n>',
-        node_decremental = '<C-m>',
-        scope_incremental = '<C-c>',
-      },
-    },
+    -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
