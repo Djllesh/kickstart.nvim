c75f670e2f489db431787ec8996f696f548a1151
Illia <prikhodko.illia@gmail.com>
2025-04-06 22:08:37 -0500

    dashboard
    dashboard

--- lua/djllesh/plugins/snacks.lua
+++ lua/djllesh/plugins/snacks.lua
@@ -37,7 +37,28 @@ return {
         },
       },
     },
-    dashboard = {},
+    dashboard = {
+      preset = {
+        header = {
+          [[
+                                                           
+              _/  _/  _/  _/                      _/       
+         _/_/_/      _/  _/    _/_/      _/_/_/  _/_/_/    
+      _/    _/  _/  _/  _/  _/_/_/_/  _/_/      _/    _/   
+     _/    _/  _/  _/  _/  _/            _/_/  _/    _/    
+      _/_/_/  _/  _/  _/    _/_/_/  _/_/_/    _/    _/     
+             _/                                            
+          _/                                               ]],
+        },
+      },
+      sections = {
+        { section = 'header' },
+        { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+        { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+        { section = 'startup' },
+      },
+    },
     notifier = {},
   },
 
